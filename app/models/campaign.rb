c1cc7b2e7ba679feb1ef9b4411845b4b873b9057
ALEXANDER <Alexander Okereke>
2014-11-16 13:00:08 +0000

    REM bugs

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,3 +1,11 @@
 class Campaign < ActiveRecord::Base
   mount_uploader :image, ImageUploader
+  validates :title, presence: true
+  validates :target_amount, presence: true
+  validates :country, presence: true
+  validates :region, presence: true
+  validates :city, presence: true
+  validates :category, presence: true
+  validates :image, presence: true
+  validates :story, presence: true
 end
