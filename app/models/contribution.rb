9769f730d2ed94bb46fe8b33ba99ec10b8294364
ALEXANDER <Alexander Okereke>
2014-12-02 23:15:05 +0000

    final work

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -1,4 +1,10 @@
 class Contribution < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :donation_amount, presence: true
+  validates :email, presence: true
+  validates :comment, presence: true
+
   belongs_to :campaign
   belongs_to :user
 end
